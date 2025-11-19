1635c63658a0804d92877e36ba694104b471e2e8
jeff mcneely <jeff@mcneely.io>
2025-11-19 10:20:10 -0600

    remove deprecated code

---
+++ scripts/fix_table_owner.sql
@@ -0,0 +1,7 @@
+DO $$ DECLARE
+    r RECORD;
+BEGIN
+    FOR r IN SELECT tablename FROM pg_tables WHERE schemaname = 'public' LOOP
+        EXECUTE 'ALTER TABLE public.' || quote_ident(r.tablename) || ' OWNER TO honeyspeak;';
+    END LOOP;
+END $$;
